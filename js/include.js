fb62de6fab0b7873132a150e6c2287d00cd8793e
JkhTV <candzzang5283@gmail.com>
2026-03-08 17:49:32 +0900

    Feat include html, 4 page
    I seperated the header, navigation, and footer into seperate HTML files
    so that they could be loaded using JavaScript.

---
+++ js/include.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", () => {
+    // 모든 HTML 요소를 가져옵니다.
+    const elements = document.getElementsByTagName("*");
+    const array = Array.from(elements);
+
+    array.forEach(element => {
+        // 포함할 HTML 파일을 속성에서 불러옵니다.
+        const includePath = element.dataset.includePath;
+        
+        if (!includePath) {
+            return;
+        }
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                element.innerHTML = this.responseText;
+            }
+        }
+        xhr.open("GET", includePath, true);
+        xhr.send();
+    });
+});
